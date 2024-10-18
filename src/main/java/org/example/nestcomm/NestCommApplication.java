2145251d04fcd68468d396daa710ac28fe11e998
Artiom <artiom.bykovskih@gmail.com>
2024-10-18 09:58:47 +0300

    initial commit

---
+++ src/main/java/org/example/nestcomm/NestCommApplication.java
@@ -0,0 +1,13 @@
+package org.example.nestcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NestCommApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nestcomm/NestCommApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nestcomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestCommApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
